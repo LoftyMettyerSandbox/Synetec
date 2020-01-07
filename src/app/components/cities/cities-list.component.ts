ddf28771759cc0cc98bc5f58966412e32794640a
Lofty Mettyer <mettyer@gmail.com>
2020-01-07 12:57:08 +0000

    Functionally valid - but unformatted and could be improved

--- src/app/components/cities/cities-list.component.ts
+++ src/app/components/cities/cities-list.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from "@angular/core";
 import { ICity } from "../../models/city.model";
+import { HttpClient } from '@angular/common/http';
 
-@Component ({
-    selector: 'cities-list',
-    templateUrl: './cities-list.component.html',
-    styleUrls: ['./cities-list.component.css']
+@Component({
+  selector: 'cities-list',
+  templateUrl: './cities-list.component.html',
+  styleUrls: ['./cities-list.component.css']
 })
 
-export class CitiesListComponent implements OnInit{
+export class CitiesListComponent implements OnInit {
 
-    cities: ICity[];
-    constructor() {}
+  cities: ICity[];
+  constructor(private httpClient: HttpClient) { }
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+
+    this.httpClient.get("http://localhost:53538/api/cities")
+      .subscribe((data: any) => {
+        this.cities = data;
+      });
+  }
+
+  deleteCity(id) {
+
+    this.httpClient.delete("http://localhost:53538/api/cities/delete-city/" + id)
+      .subscribe(res => {
+        alert("Deleted");
+      }, err => {
+        alert("Not deleted");
+      });
+
+  }
 
 
-        
-    }
 }
